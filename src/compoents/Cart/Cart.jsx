44a68aa8360138b33160e503b738531b83c87fe6
Shafiulislam <ishafiul900@gmail.com>
2023-03-28 15:46:41 +0600

    This project  some complit

--- src/compoents/Cart/Cart.jsx
+++ src/compoents/Cart/Cart.jsx
@@ -6,10 +6,16 @@ const Cart = ({cart}) => {
     // console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        // product.quantity = product.quantity || 1;
         // console.log(product);
-        totalPrice = totalPrice + product.price;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     
     const tex = totalPrice * 7;
@@ -21,7 +27,7 @@ const Cart = ({cart}) => {
         <div className='cart'>
             <h3 className='order-summary-title'>Order Summary</h3>
             
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: $ {totalPrice}</p>
             <p>Shipping Charge: $ {totalShipping}</p>
             <p>Tex: ${totalTex.toFixed(2)}</p>
